4fb985d33e782bbf72c7dd080e83f9630be00502
Kyru Cabading <kyrusri@gmail.com>
2018-04-09 05:14:03 +0800

    Added StackNavigator w/ Basic Screens
    imported StackNavigator with Homescreen & Gallery screen

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
+// import React from 'react';
+// import { StackNavigator } from 'react-navigation';
+//
+// const App = StackNavigator({
+//   Home: { screen: HomeScreen },
+//   Profile: { screen: ProfileScreen },
+// });
+//
+// export default App;
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from './src/screens/HomeScreen';
+import Gallery from './src/screens/Gallery';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <AppNavigator/>
     );
   }
 }
+
+const AppNavigator = StackNavigator({
+  HomeScreen: {screen : HomeScreen},
+  Gallery: {screen : Gallery}
+})
 
 const styles = StyleSheet.create({
   container: {
