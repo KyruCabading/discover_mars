bfc19c0e1bd3a270067d6d60a165daa18645b20a
Kyru Cabading <kyrusri@gmail.com>
2018-04-09 05:33:03 +0800

    added Homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  Button
 } from 'react-native';
+
 
 class HomeScreen extends Component{
   render(){
     return (
       <View>
         <Text> This is the Homescreen </Text>
+        <Button
+          title="Gallery"
+          onPress={() => this.props.navigation.navigate('Gallery')}
+        />
       </View>
     );
   }
