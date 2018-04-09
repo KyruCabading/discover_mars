3ff91df33a3ea1e197804b26a842224067d6c965
Kyru Cabading <kyrusri@gmail.com>
2018-04-10 00:03:32 +0800

    refactor Navigator

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import MainScreen from './src/screens/MainScreen';
-import Gallery from './src/screens/Gallery';
+import AppNavigator from './src/navigation/Navigator';
 
 export default class App extends React.Component {
   render() {
@@ -11,10 +9,5 @@ export default class App extends React.Component {
     );
   }
 }
-
-const AppNavigator = StackNavigator({
-  MainScreen: {screen : MainScreen},
-  Gallery: {screen : Gallery}
-})
 
 AppRegistry.registerComponent('dicover_mars', () => AppNavigator);
