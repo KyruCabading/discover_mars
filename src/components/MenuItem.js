5f564df8935a5e0304f56d8370f00f97b5c82597
Kyru Cabading <kyrusri@gmail.com>
2018-04-10 00:03:53 +0800

    refactor MenuItem

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -19,30 +19,20 @@ class MenuItem extends Component{
     }
   }
 
-  static navigationOptions = {
-    title: "Missions",
-    headerStyle: {
-       backgroundColor: Colors.main
-     },
-     headerTintColor: '#fff'
-  };
-
   onPressIn(){
     Animated.timing(this.state.animatePress,{
       toValue:0.96,
       duration: 150
     }).start()
+
   }
 
   onPressOut() {
-    const { navigate } = this.props.navigation;
-
     Animated.timing(this.state.animatePress,{
       toValue:1,
       duration: 200
     }).start()
-
-    navigate('Gallery');
+    this.props.onItemPress();
   }
 
   render() {
@@ -55,7 +45,7 @@ class MenuItem extends Component{
             <Animated.View
               style={image,{transform:[{scale: this.state.animatePress}]}
             }>
-              <Image style={{width: 200, height: 100}} source={this.props.image}></Image>
+              <Image style={image} source={this.props.image}></Image>
             </Animated.View>
           </TouchableWithoutFeedback>
     )
@@ -64,8 +54,9 @@ class MenuItem extends Component{
 
 const styles = StyleSheet.create({
   image: {
-    width: 200,
-    height: 100
+    width: 300,
+    height: 150,
+    margin: 5
   }
 });
 
