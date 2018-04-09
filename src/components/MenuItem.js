2c999bd1c1fb2f12d4f05eece992d12b17590984
Kyru Cabading <kyrusri@gmail.com>
2018-04-09 13:59:53 +0800

    refactor to MenuItem

---
+++ src/components/MenuItem.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableWithoutFeedback,
+  Animated
+} from 'react-native';
+import Header from '../components/common/Header';
+import Colors from '../constants/Colors';
+
+
+class MenuItem extends Component{
+  constructor(){
+    super();
+    this.state = {
+      animatePress: new Animated.Value(1)
+    }
+  }
+
+  static navigationOptions = {
+    title: "Missions",
+    headerStyle: {
+       backgroundColor: Colors.main
+     },
+     headerTintColor: '#fff'
+  };
+
+  onPressIn(){
+    Animated.timing(this.state.animatePress,{
+      toValue:0.95,
+      duration: 200
+    }).start()
+  }
+
+  onPressOut() {
+    Animated.timing(this.state.animatePress,{
+      toValue:1,
+      duration: 200
+    }).start()
+    this.props.navigation.navigate('Gallery');
+  }
+
+  render() {
+    const { image } = styles;
+    return (
+          <TouchableWithoutFeedback
+            onPressIn={() => this.onPressIn()}
+            onPressOut={() => this.onPressOut()}
+          >
+            <Animated.View
+              style={image,{transform:[{scale: this.state.animatePress}]}
+            }>
+              <Image style={{width: 200, height: 100}} source={this.props.image}></Image>
+            </Animated.View>
+          </TouchableWithoutFeedback>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 100
+  }
+});
+
+export default MenuItem;
