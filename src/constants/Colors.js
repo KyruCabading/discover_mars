1819a011b28a336d4de0e68ab0b78634d567528d
Kyru Cabading <kyrusri@gmail.com>
2018-04-09 13:33:39 +0800

    added Header style

---
+++ src/constants/Colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+  main: '#c6905b',
+  secondary: '#d8b58a',
+  secondaryLight: '#e7d4b4',
+
+};
+
+export default Colors;
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -3,27 +3,54 @@ import {
   View,
   Text,
   StyleSheet,
-  Button
+  Button,
+  Image,
+  TouchableWithoutFeedback
 } from 'react-native';
 import Header from '../components/common/Header';
+import Colors from '../constants/Colors';
 
 
 class MainScreen extends Component{
   static navigationOptions = {
     title: "Missions",
+    headerStyle: {
+       backgroundColor: Colors.main
+     },
+     headerTintColor: '#fff'
   };
 
-  render(){
+  render() {
     return (
-      <View>
-        <Text> This is the Homescreen </Text>
-        <Button
-          title="Gallery"
-          onPress={() => this.props.navigation.navigate('Gallery')}
-        />
-      </View>
-    );
+      <TouchableWithoutFeedback>
+        <View style={styles.container}>
+          <View style={{width: 200, height: 100}}>
+            <Image style={{width: 200, height: 100}} source={require('../../assets/images/opportunity.jpg')}></Image>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    )
   }
+  // render(){
+  //   return (
+  //     <View>
+  //       <Text> This is the Homescreen </Text>
+  //       <Button
+  //         title="Gallery"
+  //         onPress={() => this.props.navigation.navigate('Gallery')}
+  //       />
+  //     </View>
+  //   );
+  // }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default MainScreen;
