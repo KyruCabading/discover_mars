3d7c71b0b3c3fcde593aaab56fd429563087ec1c
Kyru Cabading <kyrusri@gmail.com>
2018-04-11 02:00:25 +0800

    add 3 column gallery

--- src/screens/Gallery.js
+++ src/screens/Gallery.js
@@ -1,29 +1,79 @@
 import React, { Component } from 'react';
+import API from '../config/api';
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList,
+  Dimensions
 } from 'react-native';
+import ImageElement from '../components/ImageElement.js';
+import roverData from '../constants/roverData.json';
+
 
 class Gallery extends Component{
-  // componentDidMount() {
-  //   this.makeRemoteRequest();
-  // }
-  //
-  // makeRemoteRequest = () => {
-  //   // Fetch Method
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      modalVisible: false,
+      modalImage: null,
+      roverData: roverData.photos,
+      columns: 3
+    }
+  }
+
+  renderItem = ({item}) => {
+    const { columns } = this.state,
+          WINDOW_WIDTH = Dimensions.get('window').width,
+          itemDimension = (WINDOW_WIDTH-(18*columns))/columns
+    return (
+      <ImageElement
+        columns={this.state.columns}
+        itemDimension={itemDimension}
+        imgsource={item.img_src}
+      />
+    )
+  };
 
   render(){
-    const { params } = this.props.navigation.state;
-    const roverId = params ? params.roverId : null;
+    const { columns } = this.state
 
-    return (
-      <View>
-        <Text>Rover Id: {JSON.stringify(roverId)}</Text>
+    return(
+      <View style={styles.container}>
+        <FlatList
+          numColumns={columns}
+          data={roverData.photos}
+          renderItem={this.renderItem}
+          keyExtractor={item => item.id.toString()}
+        />
       </View>
-    );
+    )
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
+
 export default Gallery;
+
+
+// componentDidMount() {
+//   API.fetchData();
+// }
+
+// render(){
+//   const { params } = this.props.navigation.state;
+//   const roverId = params ? params.roverId : null;
+//
+//   return (
+//     <View>
+//       <Text>Rover Id: {JSON.stringify(roverId)}</Text>
+//     </View>
+//   );
+// }
